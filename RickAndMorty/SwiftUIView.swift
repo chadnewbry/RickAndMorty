0f1900dd9405410c3dcad610645dc6960bc72a76
Chad Newbry <chad.newbry@gmail.com>
2024-10-24 13:44:46 -0400

    Project changes

---
+++ RickAndMorty/SwiftUIView.swift
@@ -0,0 +1,25 @@
+//
+//  SwiftUIView.swift
+//  RickAndMorty
+//
+//  Created by Chad Newbry on 10/24/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Text("Hello, World!")
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+        
+        // A much larger view that we can run against
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
